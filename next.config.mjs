33f4d41c4db639bd1f4beaac3b7e80583bc99458
nisit.jiraruwat <nisit.jiraruwat@gmail.com>
2022-01-29 19:59:25 +0700

    run with nextjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import withKeystone from '@keystone-6/core/next'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+}
+
+export default withKeystone(nextConfig)
